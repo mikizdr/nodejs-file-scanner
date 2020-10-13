ed1c95661c36ac576beeac0b5a0f54dd787550d5
DonMikele <miroslav.zdravkovic@gmail.com>
2020-10-13 22:21:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'slike');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        const filePath = path.join(directoryPath, file); 
+        const stats = fs.statSync(filePath)
+        console.log(stats.mtime)
+    });
+});
